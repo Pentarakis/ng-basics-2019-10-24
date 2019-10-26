f238cee5f56eb516c8df3587a8db9ccfd21bb0dd
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-10-26 14:27:16 +0200

    Lifecylce-Hooks und Destroy-Subject

--- src/app/characters/character/character.component.ts
+++ src/app/characters/character/character.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Character } from '../model/character';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../character.service';
-import { filter, pluck, switchMap } from 'rxjs/operators';
+import { filter, pluck, switchMap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ngb-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
 
   character: Character = new Character();
-
   isCreateMode = true;
+  destroy: Subject<boolean> = new Subject<boolean>();
 
   constructor(private route: ActivatedRoute, private characterService: CharacterService) {
 
@@ -21,7 +22,8 @@ export class CharacterComponent implements OnInit {
       .pipe(
         pluck('id'),
         filter(id => id !== 'create'),
-        switchMap(id => this.characterService.read(id))
+        switchMap(id => this.characterService.read(id)),
+        takeUntil(this.destroy)
       )
       .subscribe(character => {
         if (character) {
@@ -33,6 +35,11 @@ export class CharacterComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.complete();
   }
 
 }
